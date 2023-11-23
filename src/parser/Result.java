a7c4874acd8d86d3ba89a0317afb65a29199e399
GouruRK <gouru.rk.gouru.rk@gmail.com>
2023-11-23 17:15:57 +0100

    adding eclipse documentation + Mr Forax's lexer

---
+++ src/parser/Result.java
@@ -0,0 +1,10 @@
+package parser;
+
+import java.util.Objects;
+
+public record Result(Token token, String content) {
+  public Result {
+    Objects.requireNonNull(token);
+    Objects.requireNonNull(content);
+  }
+}
---
+++ src/parser/Token.java
@@ -0,0 +1,20 @@
+package parser;
+
+public enum Token {
+  IDENTIFIER("[A-Za-z]+"),
+  NUMBER("[0-9]+"),
+  LEFT_PARENS("\\("),
+  RIGHT_PARENS("\\)"),
+  LEFT_BRACKET("\\["),
+  RIGHT_BRACKET("\\]"),
+  COMMA(","),
+  COLON(":"),
+  QUOTE("\"\"\"[^\"]+\"\"\""),
+  ;
+
+  public final String regex;
+
+  Token(String regex) {
+    this.regex = regex;
+  }
+}
