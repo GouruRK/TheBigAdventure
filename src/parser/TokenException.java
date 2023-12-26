3a967ae7a273658f1a798f9871344e940c98300f
GouruRK <gouru.rk.gouru.rk@gmail.com>
2023-12-26 17:20:54 +0100

    begin parser improvements

--- src/parser/TokenException.java
+++ src/parser/TokenException.java
@@ -1,7 +1,21 @@
 package parser;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 @SuppressWarnings("serial")
 public class TokenException extends Exception { 
+  
+  private static final ArrayList<String> MESSAGE_STACK = new ArrayList<String>();
+  
+  public static void addMessage(String message) {
+    MESSAGE_STACK.add(message);
+  }
+  
+  public TokenException() {
+    super(MESSAGE_STACK.stream().collect(Collectors.joining("\n")));
+  }
+  
   public TokenException(String errorMessage) {
       super(errorMessage);
   }
